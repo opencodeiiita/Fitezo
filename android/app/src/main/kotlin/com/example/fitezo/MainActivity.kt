857cea30386af559529b36f0f4d06b8f9df49fab
Harshsharma44 <harshhash322@gmail.com>
2022-12-14 14:05:37 +0530

    Base project added

---
+++ android/app/src/main/kotlin/com/example/fitezo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fitezo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+
+void main(){
+  runApp(myApp());
+}
+
+class myApp extends StatefulWidget {
+  const myApp({Key? key}) : super(key: key);
+
+  @override
+  State<myApp> createState() => _myAppState();
+}
+
+class _myAppState extends State<myApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(title: Text("Fitezo",style: TextStyle(fontSize: 25,color: Colors.white70 , fontWeight: FontWeight.bold),),
+        backgroundColor: Colors.black,),
+        body: Container(
+          width: double.infinity,
+          height: 500,
+          child: Center(
+           child: Text("Welcome",style: TextStyle(fontSize: 30),)
+          ),
+        ),
+      ),
+    );
+  }
+}
