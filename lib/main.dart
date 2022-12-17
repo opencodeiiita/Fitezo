f4a68e02270bb455bf2d564fc16e096402a9afa6
anjalii14 <anjjalisingh14@gmail.com>
2022-12-18 03:01:02 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -17,15 +17,31 @@ class _myAppState extends State<myApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(title: Text("Fitezo",style: TextStyle(fontSize: 25,color: Colors.white70 , fontWeight: FontWeight.bold),),
-        backgroundColor: Colors.black,),
-        body: Container(
-          width: double.infinity,
-          height: 500,
-          child: Center(
-           child: Text("Welcome",style: TextStyle(fontSize: 30),)
-          ),
+      home: SafeArea(
+        child: Scaffold(
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                margin: const EdgeInsets.only(top: 30.0),
+                color: Colors.black,
+                child: const SizedBox(
+                  height: 50.0,
+                  child: Text('Fitezo', style: TextStyle(fontSize: 25,color: Colors.white70 , fontWeight: FontWeight.bold) ),
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Container(
+                  width: double.infinity,
+                  height: 500,
+                  child: const Center(
+                   child: Text("Welcome",style: TextStyle(fontSize: 30),)
+                  ),
+                ),
+              ),
+            ],
+          )
         ),
       ),
     );
