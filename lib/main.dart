d1b9888f85b721597b447bcec2b3cdf0a8effe24
Shubhankit-Pandey <shubhankitpandey15@gmail.com>
2022-12-22 21:41:05 +0530

    Added Drawer

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,7 @@ void main() {
 }
 
 class myApp extends StatefulWidget {
-  
-   myApp({Key? key}) : super(key: key);
+  myApp({Key? key}) : super(key: key);
 
   @override
   State<myApp> createState() => _myAppState();
@@ -16,46 +15,94 @@ class myApp extends StatefulWidget {
 class _myAppState extends State<myApp> {
   @override
   Widget build(BuildContext context) {
-    final List conti=['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20'];
+    final List conti = [
+      '1',
+      '2',
+      '3',
+      '4',
+      '5',
+      '6',
+      '7',
+      '8',
+      '9',
+      '10',
+      '11',
+      '12',
+      '13',
+      '14',
+      '15',
+      '16',
+      '17',
+      '18',
+      '19',
+      '20'
+    ];
     return MaterialApp(
-      
       debugShowCheckedModeBanner: false,
       home: SafeArea(
         child: Scaffold(
-            body: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-    
-            Container(
-              width: double.infinity,
-              height: 60.0,
-              color: Colors.black,
-              padding: const EdgeInsets.symmetric(horizontal: 10.0),
-              child: Row(
-                // ignore: prefer_const_literals_to_create_immutables
-                children: <Widget>[
-                  const Text('Fitezo',
-                        style: TextStyle(
-                            fontSize: 25,
-                            color: Colors.white70,
-                            fontWeight: FontWeight.bold)),  
-                ],
+          appBar: AppBar(
+            title: const Text(
+              'Fitezo',
+              style: TextStyle(
+                  fontSize: 25,
+                  color: Colors.white70,
+                  fontWeight: FontWeight.bold),
+            ),
+            backgroundColor: Colors.black,
+          ),
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(
+                child: ListView.builder(
+                    itemCount: conti.length,
+                    itemBuilder: (context, index) {
+                      return ListContainer(
+                        con: conti[index],
+                      );
+                    }),
               ),
+            ],
+          ),
+          drawer: Drawer(
+            child: ListView(
+              // padding: const EdgeInsets.all(10),
+              children: const [
+                DrawerHeader(
+                  child: Text(""),
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage("../assets/VedantFitezo.png"),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                ListTile(
+                  leading: const Icon(
+                    Icons.tips_and_updates,
+                    color: Color.fromARGB(255, 81, 18, 18),
+                  ),
+                  title: const Text('Tips'),
+                ),
+                ListTile(
+                  leading: const Icon(
+                    Icons.person,
+                    color: Color.fromARGB(255, 81, 18, 18),
+                  ),
+                  title: const Text('Profile'),
+                ),
+                ListTile(
+                  leading: const Icon(
+                    Icons.settings,
+                    color: Color.fromARGB(255, 81, 18, 18),
+                  ),
+                  title: const Text('Settings'),
+                ),
+              ],
             ),
-           
-          Expanded(
-            child: ListView.builder(
-            itemCount: conti.length,
-            itemBuilder: (context, index)
-            {
-                return ListContainer(
-                con: conti[index],
-              );
-            }
-            ),
-          )
-          ],
-        )),
+          ),
+        ),
       ),
     );
   }
