ecf330fb9a792a4d71b9070d0dbb1303c0dd238e
Bhumika Kalaru <bhumikakalaru@gmail.com>
2022-12-29 16:13:48 +0530

    Created Profile page

--- lib/main.dart
+++ lib/main.dart
@@ -15,18 +15,29 @@ class myApp extends StatefulWidget {
 class _myAppState extends State<myApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: Profile()
-        // Scaffold(
-        //   appBar: AppBar(title: Text("Fitezo",style: TextStyle(fontSize: 25,color: Colors.white70 , fontWeight: FontWeight.bold),),
-        //   backgroundColor: Colors.black,),
-        //   body: Container(
-        //     width: double.infinity,
-        //     height: 500,
-        //     child: Center(
-        //      child: Text("Welcome",style: TextStyle(fontSize: 30),)
-        //     ),
-        //   ),
-        // ),
-        );
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "Fitezo",
+            style: TextStyle(
+                fontSize: 25,
+                color: Colors.white70,
+                fontWeight: FontWeight.bold),
+          ),
+          backgroundColor: Colors.black,
+        ),
+        body: Container(
+          width: double.infinity,
+          height: 500,
+          child: Center(
+              child: Text(
+            "Welcome",
+            style: TextStyle(fontSize: 30),
+          )),
+        ),
+      ),
+    );
   }
 }
